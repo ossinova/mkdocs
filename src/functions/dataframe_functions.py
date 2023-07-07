1cbbb500c77b081beab0e10bd853e9e23b43fab3
Oscar Dyremyhr <oscar.dyremyhr@gmail.com>
2023-07-07 16:35:13 +0200

    Some major changes

---
+++ src/functions/dataframe_functions.py
@@ -0,0 +1,14 @@
+"""This module contains helper functions for the example package.
+
+"""
+
+def is_column_present(column: str) -> bool:
+    """A function that checks if a column is present in a dataframe
+
+    Arguments:
+        column: The column to check
+    Returns:
+        True if the column is present, False otherwise
+    
+    """
+    return True
---
+++ src/functions/helper_functions.py
@@ -0,0 +1,37 @@
+"""This module contains helper functions for the example package.
+
+"""
+
+
+def sum(a: int, b: int) -> int:
+    """A function that adds two numbers
+    Arguments:
+        a: The first number
+        b: The second number
+    Returns:
+        The sum of the two numbers
+    """
+    return a + b
+
+
+
+def sumB(a: int, b: int) -> int:
+    """A function that adds two numbers
+    Arguments:
+        a: The first number
+        b: The second number
+    Returns:
+        The sum of the two numbers
+    """
+    return a + b
+
+
+def sumC(a: int, b: int) -> int:
+    """A function that adds two numbers
+    Arguments:
+        a: The first number
+        b: The second number
+    Returns:
+        The sum of the two numbers
+    """
+    return a + b
